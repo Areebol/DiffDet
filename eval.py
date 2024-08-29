87724e8d4d453b8b1caa436e3866addb4238706f
Franklin Jiang <Chief@Jiangyj.Tech>
2024-08-29 18:35:23 +0800

    正常运行 eval.py

--- eval.py
+++ eval.py
@@ -1,26 +1,41 @@
 from utils import *
 from model import ViT
 
-if __name__ == "__main__":
 
-    # 加载模型
+def load_vit_model():
     vit_model_path = f"{cwd}/models/vit.pth"
-
     vit_model = ViT(d_model=2048, num_heads=8, num_classes=2)
-    vit_model.load_state_dict(torch.load(vit_model_path))
+    vit_model.load_state_dict(
+        torch.load(vit_model_path, map_location=device, weights_only=True)
+    )
+    vit_model.to(device)
+    return vit_model
 
-    # 测试模型
-    path = f"{cwd}/kaggle/working/test/sora/tokyo-walk_1"
+
+def eval_from_video_frames(video_frames_dir):
+    video_frames_paths = sorted(glob(f"{video_frames_dir}/*"))
+
+    # 获取测试数据的 CLIP 特征
     test_data = np.array(
         [
-            clip_feature(Image.open(f"{path}/frame0.jpg")).cpu().detach().numpy(),
-            clip_feature(Image.open(f"{path}/frame1.jpg")).cpu().detach().numpy(),
-            clip_feature(Image.open(f"{path}/frame2.jpg")).cpu().detach().numpy(),
-            clip_feature(Image.open(f"{path}/frame3.jpg")).cpu().detach().numpy(),
+            clip_feature(Image.open(video_frames_paths[i])).cpu().detach().numpy()
+            for i in range(4)
         ]
-    )
+    ).reshape(1, 1, 2048)
+    test_data = torch.tensor(test_data, dtype=torch.float32).to(device)
 
-    test_data = test_data.reshape(1, 1, 2048)
+    # 加载模型
+    print("加载 ViT 模型 ...")
+    vit_model = load_vit_model()
+
+    # 测试模型
     vit_model.eval()
-    data = vit_model(torch.tensor(test_data).to(device).to(torch.float32))
-    print(np.argmax(data.cpu().detach().numpy()))
+    with torch.no_grad():
+        result = vit_model(test_data)
+
+    print(f"模型输出: {result.cpu().detach().numpy()}")
+    print(f"预测结果: {np.argmax(result.cpu().detach().numpy())}")
+
+
+if __name__ == "__main__":
+    eval_from_video_frames(f"{cwd}/realfake-video-dataset/test_0")
