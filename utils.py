84d5cae22206ca4fe37e7485fdb9a07f2ea1819e
Franklin Jiang <Chief@Jiangyj.Tech>
2024-08-29 17:30:14 +0800

    重新组织文件结构

---
+++ utils.py
@@ -0,0 +1,49 @@
+import os
+import numpy as np
+import pandas as pd
+from glob import glob
+from pathlib import Path
+from tqdm import tqdm
+import matplotlib.pyplot as plt
+import torch
+import torchvision
+from torch import nn
+from torch.utils.data import Dataset, DataLoader
+from torchvision import transforms
+from torchinfo import summary
+import clip
+from PIL import Image
+
+from going_modular.going_modular import data_setup, engine
+
+import logging
+
+logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(message)s")
+
+info = logging.info
+
+cwd = Path(__file__).parent
+device = "cuda" if torch.cuda.is_available() else "cpu"
+clip_model, clip_preprocess = None, None
+
+# 图像输入尺寸
+input_shape = (224, 224)
+
+# 图像预处理转换
+transform = transforms.Compose(
+    [
+        transforms.Resize(input_shape),
+    ]
+)
+
+
+def clip_feature(img):
+    # 按需加载 CLIP 模型
+    global clip_model, clip_preprocess
+    if clip_model is None or clip_preprocess is None:
+        clip_model, clip_preprocess = clip.load("ViT-B/32", device=device)
+
+    # 图像预处理后用 CLIP 提取特征
+    img = clip_preprocess(transform(img)).unsqueeze(0).to(device)
+    features = clip_model.encode_image(img)
+    return features
