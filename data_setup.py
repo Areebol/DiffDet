06b769b1da00a30b09464d4bfd83d9b7b0854f4f
Franklin Jiang <Chief@Jiangyj.Tech>
2024-09-09 19:32:41 +0800

    调整代码

--- data_setup.py
+++ data_setup.py
@@ -1,8 +1,19 @@
 from utils import *
-from torch.utils.data import Dataset, DataLoader, ConcatDataset, random_split
+from torch.utils.data import (
+    Dataset,
+    DataLoader,
+    ConcatDataset,
+    random_split,
+    default_collate,
+)
+
+NUM_FRAMES = 8
+FEATURE_LEN = NUM_FRAMES * 512
+FEATURE = "clip"
 
 DATASETS_DIR = "/root/jyj/proj/decof/datasets"
 FEATURES_DIR = "/root/jyj/proj/decof/features"
+
 dataset_paths = {
     "DynamicCrafter": f"{DATASETS_DIR}/genvideo/train/fake/DynamicCrafter",
     "Latte": f"{DATASETS_DIR}/genvideo/train/fake/Latte",
@@ -21,7 +32,7 @@ dataset_paths = {
     "Sora": f"{DATASETS_DIR}/genvideo/val/fake/Sora",
     "MSR-VTT": f"{DATASETS_DIR}/genvideo/val/real/MSR-VTT",
 }
-FEATURE = "clip"
+
 
 # 图像预处理转换
 transform = transforms.Compose(
@@ -30,81 +41,20 @@ transform = transforms.Compose(
     ]
 )
 
-NUM_FRAMES = 8
-FEATURE_LEN = NUM_FRAMES * 512
-
+# 特征提取 CLIP 模型
 clip_model, clip_preprocess = clip.load("ViT-B/32", device=device)
 
 
-def clip_feature(img: Image) -> torch.Tensor:
-    """提取图像的 CLIP 特征"""
-    # 对图像进行预处理
-    img = clip_preprocess(transform(img)).unsqueeze(0).to(device)
-    # 用 CLIP 提取特征
-    features = clip_model.encode_image(img)
-    return features  # torch.Size([1, 512])
-
-
-# # 从视频帧中构建数据集
-# class VideoDetectionDatasetV1(Dataset):
-#     def __init__(self, paths, labels):
-#         super().__init__()
-#         self.paths = paths
-#         self.labels = labels
-
-#     def __len__(self):
-#         return len(self.paths)
-
-#     def __getitem__(self, idx):
-#         if torch.is_tensor(idx):
-#             idx = idx.tolist()
-#         frames = glob(os.path.join(self.paths[idx], "*"))
-#         label = self.labels[idx]
-#         return {"frames": frames, "fake": label, "path": self.paths[idx]}
-
-
-# # 从 NumPy 对象中加载数据集
-# class VideoDetectionDatasetV2(Dataset):
-#     def __init__(self, input, labels):
-#         super().__init__()
-#         self.inputs = input
-#         self.labels = labels
-
-#     def __len__(self):
-#         return len(self.inputs)
-
-#     def __getitem__(self, idx):
-#         if torch.is_tensor(idx):
-#             idx = idx.tolist()
-#         # 返回数据类型为 torch.float32 的 input
-#         # (1, 2048), ()
-#         # tensor([[ 0.3582,  0.1869,  0.0369,  ...,  0.6187, -0.3901,  0.2335]]), 0
-#         return torch.tensor(self.inputs[idx], dtype=torch.float32), self.labels[idx]
-
-
-# # 从 NumPy 对象中加载数据集（传入标签）
-# class VideoDetectionDatasetV3(Dataset):
-#     def __init__(self, input, label):
-#         super().__init__()
-#         self.inputs = input
-#         self.label = label  # 0 (real) / 1 (fake)
-
-#     def __len__(self):
-#         return len(self.inputs)
-
-#     def __getitem__(self, idx):
-#         return torch.tensor(self.inputs[idx], dtype=torch.float32), self.label
-
-
 class VideoFeatureDataset(Dataset):
-    def __init__(self, dataset_path, feature="clip"):
+    def __init__(self, dataset_name: str, feature="clip"):
         super().__init__()
-        self.dataset_path = dataset_path
-        self.files = list(Path(dataset_path).glob("*"))
-        self.fake = 1 if "/fake/" in str(dataset_path) else 0
+        self.dataset_name = dataset_name
+        self.dataset_path = dataset_paths[dataset_name]
+        self.files = list(Path(self.dataset_path).glob("*"))
+        self.fake = 1 if "/fake/" in str(self.dataset_path) else 0
         self.feature = feature
         info(
-            f"[数据集] 初始化数据集 {dataset_path}，样本数量：{len(self)}，标签：{self.fake}"
+            f"[数据集] 初始化数据集 {self.dataset_name}，总样本数量：{len(self)}，标签：{self.fake}"
         )
 
     def __len__(self):
@@ -136,7 +86,9 @@ class VideoFeatureDataset(Dataset):
                     error(f"[数据集] 原特征形状：{features_tensor.shape}")
                     error(f"[数据集] 原特征：{features_tensor}")
                     # 删除视频和特征文件
-                    info(f"[数据集] 删除视频和特征文件：{feature_path.with_suffix(".npy")}")
+                    info(
+                        f"[数据集] 删除视频和特征文件：{feature_path.with_suffix('.npy')}"
+                    )
                     self.files[idx].unlink()
                     feature_path.with_suffix(".npy").unlink()
                     return None
@@ -180,7 +132,7 @@ class VideoFeatureDataset(Dataset):
                 error(f"[数据集] 转换特征为 NumPy 数组出错：{e}")
                 error(f"[数据集] 提取特征：{features}")
                 # 删除视频和特征文件
-                info(f"[数据集] 删除视频和特征文件：{feature_path.with_suffix(".npy")}")
+                info(f"[数据集] 删除视频和特征文件：{feature_path.with_suffix('.npy')}")
                 self.files[idx].unlink()
                 feature_path.with_suffix(".npy").unlink()
                 return None
@@ -201,6 +153,9 @@ class VideoFeatureDataset(Dataset):
             error(f"[数据集] 加载数据集 {self.dataset_path}[{idx}] 出错：{e}")
             return None
 
+    def __repr__(self) -> str:
+        return f"{self.dataset_name} ({len(self)})"
+
 
 # 切片子数据集
 class SubsetVideoFeatureDataset(Dataset):
@@ -209,7 +164,9 @@ class SubsetVideoFeatureDataset(Dataset):
         self.dataset = dataset
         # 有效 indices
         self.indices = sorted(list(set(indices) & set(range(len(dataset)))))
-        info(f'[数据集] 初始化子数据集 {dataset.dataset_path}，样本数量：{len(self)}，标签：{self.dataset.fake}')
+        info(
+            f"[数据集] 初始化子数据集 {dataset.dataset_path}，样本数量：{len(self)}，标签：{self.dataset.fake}"
+        )
 
     def __len__(self):
         return len(self.indices)
@@ -217,44 +174,17 @@ class SubsetVideoFeatureDataset(Dataset):
     def __getitem__(self, idx):
         return self.dataset[self.indices[idx]]
 
-
-# def batch_get_features_v1(video_detection_dataset_v1: VideoDetectionDatasetV1):
-#     clip_features = []
-#     for i in tqdm(range(len(video_detection_dataset_v1))):
-#         # 载入单个样本
-#         sample = video_detection_dataset_v1[i]
-
-#         # 计算单个样本的 CLIP 特征
-#         sample_features = []
-#         for i in range(0, 4):
-#             frame_path = os.path.join(sample["path"], f"frame{i}.jpg")
-#             frame = Image.open(frame_path)
-#             frame_features = clip_feature(frame)
-#             sample_features.append(frame_features.detach().cpu().numpy())
-
-#         clip_features.append(np.array(sample_features))
-
-#     labels = [0 if d["fake"] == 0 else 1 for d in video_detection_dataset_v1]
-#     return clip_features, labels
+    def __repr__(self) -> str:
+        return f"{self.dataset.dataset_name} ({len(self)})"
 
 
-# def clip_features_from_dataset(dataset_dir: str) -> np.ndarray:
-#     info(f"[CLIP 特征] 从数据集中提取特征：{dataset_dir}")
-#     samples_path = glob(f"{dataset_dir}/*")
-#     info(f"[CLIP 特征] 数据集样本数量：{len(samples_path)}")
-#     clip_features = []
-#     for i, sample_path in enumerate(tqdm(samples_path)):
-#         frames_path = sorted(glob(f"{sample_path}/*"))
-#         sample_features = []
-#         for frame_path in frames_path:
-#             frame = Image.open(frame_path)
-#             frame_feature = clip_feature(frame)
-#             sample_features.append(frame_feature.detach().cpu().numpy())
-#         clip_features.append(np.array(sample_features))
-
-#     clip_features = np.array(clip_features)
-#     info(f"[CLIP 特征] 数据集特征尺寸：{clip_features.shape}")
-#     return clip_features
+def clip_feature(img: Image) -> torch.Tensor:
+    """提取图像的 CLIP 特征"""
+    # 对图像进行预处理
+    img = clip_preprocess(transform(img)).unsqueeze(0).to(device)
+    # 用 CLIP 提取特征
+    features = clip_model.encode_image(img)
+    return features  # torch.Size([1, 512])
 
 
 def split_dataset(dataset: Dataset):
@@ -270,7 +200,7 @@ def split_dataset(dataset: Dataset):
 # 使用 collate_fn 过滤无效数据（数据值为 None）
 def collate_fn(batch):
     batch = list(filter(lambda x: x is not None, batch))
-    return torch.utils.data.dataloader.default_collate(batch) if batch else None
+    return default_collate(batch) if batch else None
 
 
 def dataloader(dataset: Dataset):
@@ -290,7 +220,7 @@ if __name__ == "__main__":
 
     # # 加载所有数据集
     for dataset_name, dataset_path in dataset_paths.items():
-        dataset = VideoFeatureDataset(dataset_path)
+        dataset = VideoFeatureDataset(dataset_name)
         sub_dataset = SubsetVideoFeatureDataset(dataset, list(range(30000)))
         for data in sub_dataset:
             data
