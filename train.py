a124da8eaa0a6c799130e0a256fe0aaa1ab7451a
Franklin Jiang <Chief@Jiangyj.Tech>
2024-09-09 19:41:00 +0800

    调整代码

--- train.py
+++ train.py
@@ -44,56 +44,22 @@ def train(dataset: str, num_samples: int = 1000):
     info(f"[DoCoF] 训练集：{dataset}")
 
     dataset_fake = SubsetVideoFeatureDataset(
-        VideoFeatureDataset(dataset_paths[dataset]), list(range(num_samples))
+        VideoFeatureDataset(dataset), list(range(num_samples))
     )
     dataset_real = SubsetVideoFeatureDataset(
-        VideoFeatureDataset(dataset_paths["MSR-VTT"]), list(range(num_samples))
+        VideoFeatureDataset("MSR-VTT"), list(range(num_samples))
     )
     # 合并数据集
-    dataset = torch.utils.data.ConcatDataset([dataset_fake, dataset_real])
+    dataset = ConcatDataset([dataset_fake, dataset_real])
 
     # 分割数据集
     train_dataset, val_dataset = split_dataset(dataset)
 
+    # 训练模型
     model = _train(train_dataset, val_dataset)
 
     # 保存模型
     torch.save(model.state_dict(), f"{cwd}/models/{dataset}.pth")
-
-
-# def train_on(train_features_fake_path: str, test_features_path: str):
-#     info(f"[DoCoF] 训练集：{train_features_fake_path}")
-#     info(f"[DoCoF] 测试集：{test_features_path}")
-
-#     train_features_fake = np.load(train_features_fake_path)
-#     train_features_real = np.load(f"{cwd}/out/clip_feature/real/MSVD.npy")
-#     test_features = np.load(test_features_path)
-
-#     train_dataset_fake = VideoDetectionDatasetV3(train_features_fake, label=1)
-#     train_dataset_real = VideoDetectionDatasetV3(train_features_real, label=0)
-#     train_dataset = torch.utils.data.ConcatDataset(
-#         [train_dataset_fake, train_dataset_real]
-#     )
-#     test_label = 1 if "/fake/" in test_features_path else 0
-#     info(f"[DoCoF] 测试集标签：{test_label}")
-#     test_dataset = VideoDetectionDatasetV3(test_features, label=test_label)
-
-#     train_dataloader = dataloader(train_dataset)
-#     test_dataloader = dataloader(test_dataset)
-
-#     torch.manual_seed(seed)
-#     torch.cuda.manual_seed(seed)
-#     model = engine.train(
-#         model=vit_model,
-#         train_dataloader=train_dataloader,
-#         test_dataloader=test_dataloader,
-#         optimizer=optimizer,
-#         loss_fn=loss_fn,
-#         epochs=10,
-#         device=device,
-#     )
-
-#     # torch.save(vit_model.state_dict(), f"{cwd}/models/vit.pth")
 
 
 if __name__ == "__main__":
